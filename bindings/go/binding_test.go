bf9f0a7bd49a4184503d5f150106e96566958e69
Henry Chu <henrytech@outlook.com>
2025-03-31 18:35:31 +0800

    update tree-sitter

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_mlir_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-mlir"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_mlir "github.com/tree-sitter/tree-sitter-mlir/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
--- bindings/go/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/tree-sitter/tree-sitter-mlir
-
-go 1.22
-
-require github.com/smacker/go-tree-sitter 1f283e24f560
